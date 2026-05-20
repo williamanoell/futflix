a69e6ae743e79e2d5f8ec45f544b9e46d0a070d7
William Manoel <williammartins19@outlook.com>
2026-05-20 08:31:09 -0300

    Update futflix hosp

---
+++ docs/precache-manifest.3373ffa4cded43cd47a3da36a3b048c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bdf40b78f6e1d54d84b7c9b120380a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8931f9add44c178b5691",
+    "url": "./static/css/main.16186320.chunk.css"
+  },
+  {
+    "revision": "05550af1a1ca0e314d26",
+    "url": "./static/js/2.44ef0088.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.44ef0088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8931f9add44c178b5691",
+    "url": "./static/js/main.a84163a6.chunk.js"
+  },
+  {
+    "revision": "39bd7beee6335b2d08aa",
+    "url": "./static/js/runtime-main.0e0f67f5.js"
+  }
+]);
