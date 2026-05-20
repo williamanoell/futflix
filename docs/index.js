48eb3cfc947a1abe30a9bf727e91cbb087622e66
William Manoel <williammartins19@outlook.com>
2026-05-20 08:22:29 -0300

    Create docs/index.js

---
+++ docs/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/Home';
+
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (<div> Página 404 </div>);
+
+ReactDOM.render(
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+    <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={ Pagina404 } />
+    </Switch>
+  </BrowserRouter>,
+
+
+  document.getElementById('root')
+);
+
